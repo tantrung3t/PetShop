ff5254c30cfaaa647f2a95fc33a99043aa0195a2
tantrung3t <tantrung.dmc@gmail.com>
2022-03-04 18:09:25 +0700

    add router be 04/03/2022

--- be/app.js
+++ be/app.js
@@ -1,17 +1,31 @@
+//import lib
 const express = require('express')
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const body_Parser = require('body-parser');
+
+//cofig app and port
 const app = express()
 const port = 3003
-const cors = require('cors');
 
+//use lib
 app.use(cors());
+app.use(fileUpload());
+app.use(body_Parser.urlencoded({extended: false}));
+app.use(body_Parser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//import router by function
 require('./app/routers/test.router')(app);
+require('./app/routers/home.router')(app);
 
-
+//get image from backend
+app.get('/image/:id', (req, res) => {
+  res.download('./app/public/image/' + req.params.id);
+})
 
 
 app.listen(port, () => {

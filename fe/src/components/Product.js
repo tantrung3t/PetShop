7ed29512f92eae7f3f68cc6ebd772fd87a39c44b
Khiem Pham <pk.22092000@gmail.com>
2022-03-04 18:40:22 +0700

    dcm cuoc doi

---
+++ fe/src/components/Product.js
@@ -0,0 +1,17 @@
+import {React} from 'react';
+import {Link} from 'react-router-dom'
+
+export default function Product(props) {
+  return (
+
+    <Link to={`/${props.id}`} className="p-1">
+      <div className="home__product">
+        <img src={props.src} alt="product" width="100%" className="home__product-img"></img>
+        <div className="p-2">
+          <div className="home__product-name">{props.name}</div>
+          <div className="home__product-price">{props.price} đ</div>
+        </div>
+      </div>
+    </Link>
+  )
+}

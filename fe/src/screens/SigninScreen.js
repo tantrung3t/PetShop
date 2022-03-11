33866c9019bff7a3ab63cb4c9c90dd4d326afa58
tantrung3t <tantrung.dmc@gmail.com>
2022-03-11 11:49:42 +0700

    update sang 11/03/2022

--- fe/src/screens/SigninScreen.js
+++ fe/src/screens/SigninScreen.js
@@ -95,63 +95,49 @@ export default function Account() {
           console.log(error);
         });
     }
-
-
-
-
-<<<<<<< HEAD
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-=======
-
->>>>>>> 16076f8523029e3e1fb563bdcfedb3251a7b7692
-
-
   }
+    return (
 
-  return (
+      <div className="body-sign">
+        <div className={`container-sign ${isContainerActive}`}>
+          <div className="form-container sign-up-container">
+            <form className="form-sign" onSubmit={handleSignUp}>
+              <h1 className="h1-sign">Tạo tài khoản</h1>
+              <input name="username_sign-up" className="input-sign" type="text" placeholder="Tên đăng nhập" />
+              <input name="password_sign-up" className="input-sign" type="password" placeholder="Nhập mật khẩu" />
+              <input name="re_password_sign-up" className="input-sign" type="password" placeholder="Nhập lại mật khẩu" />
+              <input name="name_sign-up" className="input-sign" type="text" placeholder="Nhập họ tên" />
+              <input name="email_sign-up" className="input-sign" type="email" placeholder="Nhập email" />
 
-    <div className="body-sign">
-      <div className={`container-sign ${isContainerActive}`}>
-        <div className="form-container sign-up-container">
-          <form className="form-sign" onSubmit={handleSignUp}>
-            <h1 className="h1-sign">Tạo tài khoản</h1>
-            <input name="username_sign-up" className="input-sign" type="text" placeholder="Tên đăng nhập" />
-            <input name="password_sign-up" className="input-sign" type="password" placeholder="Nhập mật khẩu" />
-            <input name="re_password_sign-up" className="input-sign" type="password" placeholder="Nhập lại mật khẩu" />
-            <input name="name_sign-up" className="input-sign" type="text" placeholder="Nhập họ tên" />
-            <input name="email_sign-up" className="input-sign" type="email" placeholder="Nhập email" />
+              <button type="submit" className="button-sign my-2">Đăng ký</button>
+            </form>
+          </div>
+          <div className="form-container sign-in-container">
+            <form onSubmit={handleSignIn} className="form-sign">
+              <h1 className="h1-sign">Đăng nhập</h1>
 
-            <button type="submit" className="button-sign my-2">Đăng ký</button>
-          </form>
-        </div>
-        <div className="form-container sign-in-container">
-          <form onSubmit={handleSignIn} className="form-sign">
-            <h1 className="h1-sign">Đăng nhập</h1>
+              <input name="username" className="input-sign" type="text" placeholder="Tên tài khoản" />
+              <input name="password" className="input-sign" type="password" placeholder="Mật khẩu" />
+              <h4>Quên mật khẩu</h4>
+              <button type="submit" className="button-sign">Đăng nhập</button>
 
-            <input name="username" className="input-sign" type="text" placeholder="Tên tài khoản" />
-            <input name="password" className="input-sign" type="password" placeholder="Mật khẩu" />
-            <h4>Quên mật khẩu</h4>
-            <button type="submit" className="button-sign">Đăng nhập</button>
-
-          </form>
-        </div>
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-panel overlay-left">
-              <h1>Chào mừng trở lại!</h1>
-              <p>Để giữ kết nối với chúng tôi, vui lòng đăng nhập bằng thông tin cá nhân mà bạn đã đăng ký!</p>
-              <button onClick={changeSignIn} className="button-sign ghost" id="signIn">Đăng nhập</button>
-            </div>
-            <div className="overlay-panel overlay-right">
-              <h1>Xin chào bạn!</h1>
-              <p>Nếu bạn chưa có tài khoản, hãy thử tạo tài khoản tại đây!</p>
-              <button onClick={changeSignUp} className="button-sign ghost" id="signUp">Đăng ký</button>
+            </form>
+          </div>
+          <div className="overlay-container">
+            <div className="overlay">
+              <div className="overlay-panel overlay-left">
+                <h1>Chào mừng trở lại!</h1>
+                <p>Để giữ kết nối với chúng tôi, vui lòng đăng nhập bằng thông tin cá nhân mà bạn đã đăng ký!</p>
+                <button onClick={changeSignIn} className="button-sign ghost" id="signIn">Đăng nhập</button>
+              </div>
+              <div className="overlay-panel overlay-right">
+                <h1>Xin chào bạn!</h1>
+                <p>Nếu bạn chưa có tài khoản, hãy thử tạo tài khoản tại đây!</p>
+                <button onClick={changeSignUp} className="button-sign ghost" id="signUp">Đăng ký</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }

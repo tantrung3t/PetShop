cff0e5ae67d6290766b3c56629caa673af306fe4
tantrung3t <tantrung.dmc@gmail.com>
2022-04-20 12:01:55 +0700

    trung 12:00pm 20/04/2022

--- be/app/models/account.model.js
+++ be/app/models/account.model.js
@@ -5,7 +5,7 @@ const Account = function (account) {
 
 Account.account_orders = function (account_id, result) {
 
-    var strquery = "SELECT orders.order_id, products.product_id, products.product_name, products.product_image, products.product_price, orders.order_status, orders.order_date, orders.order_address, orders.order_payment_momo, orders_detail.orders_detail_quantity FROM `orders`, `orders_detail`, `products` WHERE orders.order_id = orders_detail.order_id and products.product_id = orders_detail.product_id and orders.account_id = " + account_id
+    var strquery = "SELECT orders.order_id, products.product_id, products.product_name, products.product_image, products.product_price, orders.order_status, orders.order_date, orders.order_address, orders.order_payment_momo,orders.order_total, orders_detail.orders_detail_quantity FROM `orders`, `orders_detail`, `products` WHERE orders.order_id = orders_detail.order_id and products.product_id = orders_detail.product_id and orders.account_id = " + account_id
 
     db.query(strquery, function (err, data) {
         if (err || data.length === 0) {
